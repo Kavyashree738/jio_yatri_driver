b844fc82e4df70d4873fe01d1920333c8479bf6d
Kavyashree738 <jioyattri@gmail.com>
2025-06-19 10:08:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import DriverDashboard from './components/DriverDashboard'
 import OwnerDashboard from './components/OwnerDashboard'
 import UserDocumentsViewer from './components/UserDocumentsViewer'
 import Home from './components/Home'
+import UserProfile from './components/UserProfile'
 import { Routes, Route } from 'react-router-dom';
+
 const App = () => {
 
 
@@ -15,6 +17,7 @@ const App = () => {
         <Route path="/orders" element={<DriverDashboard/>} />
         <Route path="/owner-dashboard" element={<OwnerDashboard/>} />
         <Route path='/my-documents' element={<UserDocumentsViewer/>}/>
+     <Route path="/profile" element={<UserProfile/>} />
       </Routes>
     </AuthProvider>
   )
