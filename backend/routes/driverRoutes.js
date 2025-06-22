186356362a8a6dcd6385677446b318f6fa1f9bc9
Kavyashree738 <jioyattri@gmail.com>
2025-06-22 17:02:42 +0530

    Update driverRoutes.js

--- backend/routes/driverRoutes.js
+++ backend/routes/driverRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { registerDriver, getDriver, updateDriverStatus,getDriverStatus ,updateDriverLocation,getDriverLocation,getAvailableShipments,registerFCMToken} = require('../controllers/driverController');
+const { registerDriver,checkDriverExists, getDriver, updateDriverStatus,getDriverStatus ,updateDriverLocation,getDriverLocation,getAvailableShipments,registerFCMToken} = require('../controllers/driverController');
 const verifyFirebaseToken = require('../middleware/verifyFirebaseToken');
 
+router.get('/check/:userId', verifyFirebaseToken, checkDriverExists);
 router.post('/register', verifyFirebaseToken, registerDriver);
 router.get('/info/:userId', verifyFirebaseToken, getDriver);
 router.get('/status', verifyFirebaseToken, getDriverStatus);
