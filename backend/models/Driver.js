87242c2139589218d03a3c3e1dba76202f68a3b6
Kavyashree738 <jioyattri@gmail.com>
2025-06-29 18:31:13 +0530

    Update Driver.js

--- backend/models/Driver.js
+++ backend/models/Driver.js
@@ -4,13 +4,24 @@ const driverSchema = new mongoose.Schema({
   userId: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   phone: { type: String, required: true },
+    profileImage: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'fs.files',
+    default: null
+  },
   vehicleType: { type: String, enum: ['TwoWheeler', 'ThreeWheeler', 'Truck'], required: true },
   vehicleNumber: { type: String, required: true },
-  status: { type: String, enum: ['active', 'inactive'], default: 'inactive' },
+   documents: {
+    license: { type: mongoose.Schema.Types.ObjectId, ref: 'fs.files' },
+    rc:      { type: mongoose.Schema.Types.ObjectId, ref: 'fs.files' }
+  },
+  status: { type: String, enum: ['active', 'inactive'], default: 'active' },
   location: {
     type: { type: String, enum: ['Point'], default: 'Point' },
     coordinates: { type: [Number], default: [0, 0] }
   },
+  isLocationActive: { type: Boolean, default: false },
+  fcmToken: { type: String, default: null }, 
   isAvailable: { type: Boolean, default: true },
   activeShipment: { type: mongoose.Schema.Types.ObjectId, ref: 'Shipment' },
   earnings: { type: Number, default: 0 },
