c2f830708b9a19bd01ef2cfc096d102369fb86c0
Kavyashree738 <jioyattri@gmail.com>
2025-08-04 10:46:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import UserProfile from './components/UserProfile'
 import DeliveryHistory from './components/DeliveryHistory'
 import { Routes, Route } from 'react-router-dom';
 import SplashScreen from './components/SplashScreen'; 
+import PrivacyPolicy from './components/PrivacyPolicy';
 const App = () => {
   return (
     <AuthProvider>
@@ -19,6 +20,7 @@ const App = () => {
         <Route path='/my-documents' element={<UserDocumentsViewer/>}/>
      <Route path="/profile" element={<UserProfile/>} />
     <Route path="/delivery-history" element={<DeliveryHistory />} />
+     <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
     </AuthProvider>
   )
