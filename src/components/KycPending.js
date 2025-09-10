d405528ebaccaaa6428db931218ddc39ecf92791
Kavyashree738 <jioyattri@gmail.com>
2025-09-10 23:06:16 +0530

    Update KycPending.js

--- src/components/KycPending.js
+++ src/components/KycPending.js
@@ -150,6 +150,9 @@ export default function KycPending() {
               <FaClock /> We haven’t received your KYC yet.
             </div>
             <p className="kyc-sub">Please submit Aadhaar and PAN in the registration form.</p>
+             <button className="btn" onClick={() => nav('/register')}>
+      Shop Registration
+    </button>
           </>
         )}
 
